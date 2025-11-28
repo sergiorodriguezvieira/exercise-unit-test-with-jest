80514f5b8bc28dffc819648f802d80c1883a0c0c
Sergio Rodriguez Vieira <sergiorodriguezvieira@gmail.com>
2025-11-28 18:54:24 +0000

    Pending changes exported from your codespace

---
+++ .learn/resets/07-running-the-test/app.js
@@ -0,0 +1,10 @@
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum };
---
+++ test.js
@@ -0,0 +1,11 @@
+// Importar la función sum del archivo app.js
+const { sum } = require('./app.js');
+
+// Comienza tu primera prueba
+test('adds 14 + 9 to equal 23', () => {
+    // Dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // Esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
